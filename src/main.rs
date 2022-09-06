5d56abb139ab5960056d9d958235cab5639cc5e4
Valentin Zwerschke <v.zwerschke@mail.de>
2022-09-06 21:27:34 +0200

    Add basic file construction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{fs, process::Command};
+
 fn main() {
     println!("Hello, world!");
+    //let mut _res = fs::create_dir("./git");
+    //_res = fs::create_dir("./git/test2.git");
+
+    //println!("{:?}", _res);
+
+
+    let status = Command::new("git")
+        .args(["init", "--bare"])
+        .current_dir("./git/test.git")
+        .status()
+        .expect("ls command failed to start");
+
+    println!("ls status: {status}")
+
 }
